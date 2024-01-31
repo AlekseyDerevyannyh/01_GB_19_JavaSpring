48b324b83151065bd962bd3d42c8ce064be5e7f0
Aleksey Derevyannyh <DerevyannyhAA@gmail.com>
2024-01-31 21:07:28 +0300

    modify HW 7

---
+++ seminar/07/hw/src/main/java/ru/gb/security/CustomPasswordEncoder.java
@@ -0,0 +1,15 @@
+package ru.gb.security;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+public class CustomPasswordEncoder implements PasswordEncoder {
+    @Override
+    public String encode(CharSequence rawPassword) {
+        return null;
+    }
+
+    @Override
+    public boolean matches(CharSequence rawPassword, String encodedPassword) {
+        return rawPassword.equals(encodedPassword);
+    }
+}
