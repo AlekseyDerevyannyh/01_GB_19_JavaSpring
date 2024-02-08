9d660ceb65df478655b4a1178c0a6a2fbd8bfd97
Aleksey Derevyannyh <DerevyannyhAA@gmail.com>
2024-02-08 15:18:11 +0300

    init HW 9

---
+++ seminar/09/hw/book-service/src/main/java/ru/gb/api/BookController.java
@@ -0,0 +1,12 @@
+package ru.gb.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/book")
+public class BookController {
+
+//    @GetMapping("")
+}
